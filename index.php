2596c9185ef04024498623227a221981af4e2599
Bentechy66 <sendbenspam@yahoo.co.uk>
2017-10-09 18:08:36 +0100

    Add optional ReCaptcha verification for login

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway"> </head>
+    <script src='https://www.google.com/recaptcha/api.js'></script>
 <style>
 	body,
 	h1,
@@ -38,17 +39,47 @@
 					<div class="w3-row">
 						<div class="w3-col m8 s12">
 							<form id="loginform" name="input" action="" method="post"> <label for="username" style="display: inline-block; width: 6em;">Username: </label><input type="text" value="" id="username" name="username" style="max-width:100%;" />
-								<p style="font-size:0px;"> <br></p> <label for="password" style="display: inline-block; width: 6em;">Password: </label><input type="password" value="" id="password" name="password" style="max-width:100%;" /> <br><br>
+								<p style="font-size:0px;"> <br></p> <label for="password" style="display: inline-block; width: 6em;">Password: </label><input type="password" value="" id="password" name="password" style="max-width:100%;" />
+                                <?php
+                                $config = include("setup/config.php");
+                                if ($config["use_recaptcha"] == "True") {
+                                    require_once "recaptchalib.php";
+                                    echo '<br><div class="g-recaptcha" data-sitekey="' . $config["captcha_site_key"] . '"></div>';
+                                }
+                                ?>
+                                <br><br>
 								<p>
 									<?php
 									$config = include("setup/config.php");https://remotehound.ddns.net/auth/
                                         if (isset($_POST["username"]) && isset($_POST["password"])){
                                             if ($_POST["username"] == $config['username'] && $_POST["password"] == $config['password']){
+                                                if ($config["use_recaptcha"] == "True") {
+                                                $response = null;
+                                                $reCaptcha = new ReCaptcha($config["captcha_api_key"]);
+                                                    // if submitted check response
+                                                    if ($_POST["g-recaptcha-response"]) {
+                                                        $response = $reCaptcha->verifyResponse(
+                                                            $_SERVER["REMOTE_ADDR"],
+                                                            $_POST["g-recaptcha-response"]
+                                                        );
+                                                    }
+                                                    
+                                                if ($response != null && $response->success) {
+                                                    setcookie("auth", $config['auth'], time() + (86400 * 30), "/");
+                                                    header("Location: /auth");
+                                                    die();
+                                                  } else {
+                                                        echo "Please check the captcha!";   
+                                                    }
+                                                    
+                                                }
+                                                else {
                                                 setcookie("auth", $config['auth'], time() + (86400 * 30), "/");
                                                 header("Location: /auth");
                                                 die();
+                                                }
                                             }else {
-                                                echo "username/password incorrect";
+                                                echo "Username/Password incorrect!";
                                             }
                                         } 
                                     ?>
--- setup/config.php
+++ setup/config.php
@@ -2,7 +2,11 @@
 return array(
     'auth' => 'Your-random-string-here',  #this is the authorisation code used for all internal authenticaion -> make it long!
     'img_save_location' => '/var/cctv', #This sets where the snapshot images are stored.
-	
+    
+    'use_recaptcha' => 'False', #If the login site should use Google Recaptcha v2.0
+	'captcha_api_key' => 'ApiKey', #The API key for your Google Recaptcha (AKA a secret key)
+    'captcha_site_key' => 'SiteKey', #The Site Key for your Google Recaptcha
+    
 	'username' => 'user', #This is the username used to log in.
 	'password' => 'pass', #This is the password used to log in.
 	
